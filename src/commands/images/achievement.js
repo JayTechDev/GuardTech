4b80b4633fddc2c47524fb592f3679f14bc44e28
Jay <82115577+Not-Jayy@users.noreply.github.com>
2023-04-30 15:25:50 +0100

    Create achievement.js

---
+++ src/commands/images/achievement.js
@@ -0,0 +1,28 @@
+const { ChatInputCommandInteraction, SlashCommandBuilder } = require('discord.js');
+const API_URL = 'https://api.alexflipnote.dev/achievement';
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('achievement')
+        .setDescription('Generate a Minecraft achievement.')
+        .setDMPermission(false)
+        .addStringOption(option => option
+                .setName('text')
+                .setDescription('The text.')
+                .setMaxLength(100)
+                .setMinLength(1)
+                .setRequired(true)
+        ),
+    /**
+     * @param {ChatInputCommandInteraction} interaction
+     */
+    async execute(interaction, client) {
+        const { options } = interaction;
+
+        const AchievementText = options.getString('text');
+
+        interaction.reply({
+            content: API_URL + `?text=${AchievementText}`
+        });
+    },
+};
