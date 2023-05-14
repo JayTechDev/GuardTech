02f14387df68af3b8a3ad68b09c476f00b769962
Jay <jayedmead2456@icloud.com>
2023-05-14 12:27:45 +0100

    more custom client things

--- src/index.js
+++ src/index.js
@@ -1,23 +1,4 @@
-console.clear();
-
 const { Assistant } = require('./util/classes/Assistant');
-const { readdirSync } = require('fs');
-const { connect } = require('mongoose');
-require('dotenv/config');
 
 const client = new Assistant();
-
-const handlerFiles = readdirSync('./src/handlers/').filter(file => file.endsWith('.js'));
-for (const file of handlerFiles) require(`./handlers/${file}`)(client);
-
-const systemFolders = readdirSync('./src/systems/');
-for (const folder of systemFolders) {
-    const systemFiles = readdirSync(`./src/systems/${folder}`).filter(file => file.endsWith('.js'));
-    for (const file of systemFiles) require(`../src/systems/${folder}/${file}`)(client);    
-};
-
-client.handleCommands();
-client.handleEvents();
-client.login(process.env.BOT_TOKEN).then(() => {
-    connect(process.env.DATABASE_URL).then(() => console.log('[Database Status]: Connected'));
-}).catch(console.error);
+client.startup(client);
--- src/util/classes/Assistant.js
+++ src/util/classes/Assistant.js
@@ -1,4 +1,7 @@
 const { Client, GatewayIntentBits, Collection, ActivityType } = require('discord.js');
+const { readdirSync } = require('fs');
+const { connect } = require('mongoose');
+require('dotenv/config');
 
 class Assistant extends Client {
     constructor() {
@@ -16,6 +19,24 @@ class Assistant extends Client {
         });
 
         this.commands = new Collection();
+    };
+
+    startup(client) {
+        console.clear();
+        console.log('------------------------------------------------------');
+
+        const HandlerFiles = readdirSync('./src/handlers/').filter(file => file.endsWith('.js'));
+        for (const file of HandlerFiles) require(`../../handlers/${file}`)(client);
+
+        const SystemFolders = readdirSync('./src/systems');
+        for (const folder of SystemFolders) {
+            const SystemFiles = readdirSync(`./src/systems/${folder}`).filter(file => file.endsWith('.js'));
+            for (const file of SystemFiles) require(`../../systems/${folder}/${file}`)(client);
+        };
+
+        client.handleCommands();
+        client.handleEvents();
+        client.login(process.env.BOT_TOKEN).then(() => { connect(process.env.DATABASE_URL).then(() => console.log('[Database Status]: Connected')) }).catch(console.error);
     };
 };
 
