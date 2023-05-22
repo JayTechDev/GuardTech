3cd5731ef292abf6b5b487d6713fefa0f213ec85
Jay <jayedmead2456@icloud.com>
2023-05-22 20:33:38 +0100

    IM GONNA DO BAD THINGS

--- src/commands/info/help.js
+++ src/commands/info/help.js
@@ -25,7 +25,7 @@ module.exports = {
             },
             {
                 name: '• Moderation',
-                value: codeBlock('ban, block, case, kick, lock, mod, mute, nick, notes, purge, rmpunish, slowmode, softban, unban, unblock, unlock, unmute, warn')
+                value: codeBlock('ban, block, case, kick, lock, mod, mute, nick, purge, rmpunish, slowmode, softban, unban, unblock, unlock, unmute, warn')
             },
             {
                 name: '• Misc',
