fc4e702a04f5eb26f0370744708efde23f826263
Jay <jayedmead2456@icloud.com>
2023-05-27 12:20:02 +0100

    ` virustotal ` command

---
+++ src/commands/info/virustotal.js
@@ -0,0 +1,43 @@
+const { ChatInputCommandInteraction, SlashCommandBuilder, EmbedBuilder, codeBlock } = require('discord.js');
+const VirusTotalApi = require('virustotal-api');
+require('dotenv/config');
+
+const VirusTotal = new VirusTotalApi(process.env.VIRUSTOTAL_KEY);
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('virustotal')
+    .setDescription('Look up a URL with Virus Total.')
+    .setDMPermission(false)
+    .addStringOption(option => option
+            .setName('url')
+            .setDescription('Look up a URL.')
+            .setRequired(true)
+    ),
+    /**
+     * @param {ChatInputCommandInteraction} interaction
+     */
+    async execute(interaction, client) {
+        const { options, user } = interaction;
+
+        const URL = options.getString('url');
+
+        VirusTotal.urlScan(URL).then((response) => {
+            const VirusTotalEmbed = new EmbedBuilder()
+            .setColor('Blue')
+            .setAuthor({ name: `${user.tag}'s Scan`, iconURL: `${user.displayAvatarURL()}` })
+            .setDescription([
+                `> **URL:** ${URL}`,
+                `> **Scan ID:** ${response.scan_id}`,
+                `> ** Scanned On:** ${response.scan_date}`,
+            ].join('\n'))
+            .setFields({
+                name: '• Results', value: `[View results](${response.permalink})`
+            })
+
+            interaction.reply({
+                embeds: [VirusTotalEmbed]
+            }); 
+        });
+    },
+};
