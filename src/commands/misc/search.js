426ff2c68f91cb2fc135e28e820921b7442eee06
shipotatoe <terrifyingcrap123@gmail.com>
2023-05-29 21:21:03 +0100

    added a google seach command

---
+++ src/commands/misc/search.js
@@ -0,0 +1,46 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const axios = require('axios');
+
+require('dotenv/config');
+
+const CX = process.env.CX;
+const SEARCH_API_KEY = process.env.SEARCH_API_KEY;
+
+module.exports = ({
+	data: new SlashCommandBuilder()
+	.setName('google')
+	.setDescription('Search the web')
+	.addStringOption(option => option
+		.setName('query')
+		.setDescription('search terms')
+		.setRequired(true)
+),
+
+	async execute(interaction){
+		const { options } = interaction;
+		const Query = options.getString('query');
+
+		await interaction.deferReply();
+
+		const response = await axios.get(`https://customsearch.googleapis.com/customsearch/v1?cx=${CX}&num=4&q=${Query}&key=${SEARCH_API_KEY}`).then( response => {
+			const titles = [];
+			const links = [];
+			for(element of response.data.items){
+				titles.push(element.title);
+				links.push(element.link)}
+			
+		
+			const embed = new EmbedBuilder()
+			.setColor(0x000000)
+			.setTitle('Search Results')
+			.addFields(
+				{name: titles[0], value: links[0]},
+				{name: titles[1], value: links[1]},
+				{name: titles[2], value: links[2]},
+				{name: titles[3], value: links[3]}
+			);
+
+		 	interaction.editReply({embeds: [embed]});
+		});
+	},
+});
