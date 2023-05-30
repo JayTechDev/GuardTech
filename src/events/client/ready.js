89b461acc8384fd90388a371ee5890f60108061a
Jay <jayedmead2456@icloud.com>
2023-05-30 09:23:00 +0100

    botinfo updates and a online notifier i guess?

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -1,7 +1,22 @@
+const { Client, EmbedBuilder, userMention } = require("discord.js");
+const { Colours } = require('../../config.json');
+
 module.exports = {
     name: 'ready',
     once: true,
-    execute() {
+    /**
+     * @param {Client} client
+     */
+    execute(client) {
         console.log('[Bot Status]: Online');
+
+        client.channels.fetch('929378716902117471').then(channel => {
+            const OnlineEmbed = new EmbedBuilder()
+            .setColor(Colours.Default_Colour)
+            .setTitle('Online')
+            .setDescription(`<:online:1100886580169822218> ${userMention(client.user.id)} is now online, if something breaks blame Jay.`)
+
+            channel.send({ embeds: [OnlineEmbed] })
+        });
     },
 };
