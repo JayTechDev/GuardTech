affc993ae0d5938370a117f5154bfea78be685d5
shipotatoe <terrifyingcrap123@gmail.com>
2023-06-03 13:09:29 +0100

    new power personality/choosing personality :SickBro:

--- src/commands/misc/jenny.js
+++ src/commands/misc/jenny.js
@@ -4,7 +4,8 @@ const wait = require('node:timers/promises').setTimeout;
 require('dotenv/config');
 
 const OPENAI_KEY = process.env.OPENAI_TOKEN;
-const OPENAI_PROMPT = process.env.PROMPT;
+const JENNY_PROMPT = process.env.JENNY_PROMPT;
+const POWER_PROMPT = process.env.POWER_PROMPT;
 
 const configuration = new Configuration({ apiKey: OPENAI_KEY });
 const openai = new OpenAIApi(configuration);
@@ -13,6 +14,14 @@ module.exports = ({
 	data: new SlashCommandBuilder()
 	.setName('jenny')
 	.setDescription('Talk to Jenny.')
+	.addStringOption(option => option
+			.setName('personality')
+			.setDescription('personality of the AI.')
+			.setRequired(true)
+			.addChoices(
+				{ name : 'Power', value: 'power' },
+				{ name: 'Jenny.ts', value: 'jenny' }
+	))
 	.addStringOption(option => option
 			.setName('message')
 			.setDescription('The prompt.')
@@ -23,11 +32,20 @@ module.exports = ({
 	 */
 	async execute(interaction){
 		const { options } = interaction;
-
+		
+		const Personality = options.getString('personality');
 		const Message = options.getString('message');
+		
+		let OPENAI_PROMPT;
+
+		if(Personality === 'power'){
+			OPENAI_PROMPT = POWER_PROMPT;	
+		}else if(Personality === 'jenny'){
+			OPENAI_PROMPT = JENNY_PROMPT;
+		};
 
 		await interaction.deferReply();
-
+		
 		const response = await openai.createChatCompletion({ model: 'gpt-3.5-turbo', messages: [{ role: 'user', content: `${OPENAI_PROMPT} ${Message}` }] });
 
 		await wait(5000);
