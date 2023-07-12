db0eae04a20b763769b9f8825a7247dbcb15de5f
Jay <jayedmead2456@icloud.com>
2023-07-12 14:11:49 +0100

    you can now reply with the say command, cool

--- src/commands/misc/say.js
+++ src/commands/misc/say.js
@@ -1,5 +1,6 @@
 const { ChatInputCommandInteraction, SlashCommandBuilder, PermissionFlagsBits, ChannelType, channelMention } = require('discord.js');
 const { Emojis } = require('../../config.json');
+const wait = require('node:timers/promises').setTimeout;
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -8,6 +9,7 @@ module.exports = {
     .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
     .setDMPermission(false)
     .addStringOption(option => option.setName('message').setDescription('What do you want the bot to say?').setRequired(true).setMaxLength(2000).setMinLength(1))
+    .addStringOption(option => option.setName('reference').setDescription('The id of the message to reply to, NOT the message link.'))
     .addChannelOption(option => option.setName('channel').setDescription('Where to send the message.').addChannelTypes(ChannelType.GuildText)),
     /**
      * @param {ChatInputCommandInteraction} interaction
@@ -16,11 +18,17 @@ module.exports = {
         const { channel, options } = interaction;
 
         const Message = options.getString('message');
+        const Reference = options.getString('reference');
         const Channel = options.getChannel('channel') || channel;
 
-        Channel.sendTyping();
-        Channel.send({ content: `${Message}`, allowedMentions: { parse: ['users'] } }).then(() => {
-            interaction.reply({ content: `${Emojis.Success_Emoji} Message sent to ${channelMention(Channel.id)}`, ephemeral: true });
-        });
+        await interaction.deferReply({ ephemeral: true });
+        
+        if (Reference) {
+            await Channel.messages.fetch(Reference).then(message => { message.reply({ content: `${Message}`, allowedMentions: { repliedUser: true } }) });
+        } else {
+            Channel.send({ content: `${Message}`, allowedMentions: { parse: ['users'] } });
+        };
+
+        await interaction.editReply({ content: 'Message has been sent.' });
     },
 };
