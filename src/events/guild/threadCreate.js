e66a2a7ace721bddb9d8186db5a8587b04493624
Jay <jayedmead2456@icloud.com>
2023-07-12 13:39:25 +0100

    message for new support posts

--- src/events/guild/threadCreate.js
+++ src/events/guild/threadCreate.js
@@ -7,5 +7,17 @@ module.exports = {
      */
     async execute(thread) {
         await thread.join();
+
+        if (thread.parentId === '1019648404445474909') {
+            thread.send({ 
+                content: [
+                    '- Explain your exact issue, the more descriptive you are the more we can help.',
+                    '- Post any screenshots/videos that are relevent to your issue.',
+                    '- Try and search other posts, Google or ChatGPT for similar problems and solutions.',
+                    '- Do not use this to report users or concerns, open a ticket for that.',
+                    '- Do not ping anyone for support, it will not help at all.'
+                ].join('\n')
+            });
+        };
     },
 };
