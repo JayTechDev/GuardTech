62d4b65ea9e5a9d0ec240b6c8b3f3e2c95f6e0bd
Jay <jayedmead2456@icloud.com>
2023-08-09 23:16:42 +0100

    actual faq command

--- src/commands/info/faq.js
+++ src/commands/info/faq.js
@@ -8,7 +8,9 @@ module.exports = {
     .setDMPermission(false)
     .addStringOption(option => option.setName('question').setDescription('The question.').setRequired(true).addChoices(
         { name: 'How do I become staff?', value: 'become-staff' },
-        { name: 'What is JayCord?', value: 'what-jaycord' }
+        { name: 'What is JayCord?', value: 'what-jaycord' },
+        { name: 'I need to report someone', value: 'reporting-user' },
+        { name: 'I want to appeal a punishment', value: 'appeal-punishment' }
     )),
     /**
      * @param {ChatInputCommandInteraction} interaction
@@ -20,10 +22,16 @@ module.exports = {
         
         switch (Question) {
             case 'become-staff':
-                interaction.reply({ content: '**How do I become staff?**\n> Wait until applications are open, that\'s pretty much it.' });
+                interaction.reply({ content: '**How do I become staff?**\n> Staff Applications will be announced whenever we are in need of more staff, asking/begging for it will not help you in any way possible so don\'t try it.' });
                 break;
             case 'what-jaycord':
-                interaction.reply({ content: '**What is JayCord?**\n> i forgot.' });
+                interaction.reply({ content: '**What is JayCord?**\n> Our entire goal of JayCord is to bring users from all different platforms together for a place to hangout and socialise with eachother. We also offer different forums for different topics like showcasing, feedback on anything you make and a whole lot more.' });
+                break;
+            case 'reporting-someone':
+                interaction.reply({ content: '**I need to report someone**\n> If you need to report someone to us please open a report ticket and explain everything that happened and we will deal with it accordingly.\n- If it is a Discord issue please report the user to discord they will deal with it.' });
+                break;
+            case 'appeal-punishment':
+                interaction.reply({ content: '**I want to appeal a punishment**\n> If you would like to appeal your ban or mute and be given a second chance head over to our appeal form located [here](https://dyno.gg/form/b72ba489).' });
                 break;
         };
     },
